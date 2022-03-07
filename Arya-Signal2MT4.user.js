8566326b07da7db2e62d1fcd8cc0ec504883d1d2
Coding Seb <codingseb@users.noreply.github.com>
2022-03-07 19:43:02 +0100

    Update Arya-Signal2MT4.user.js

--- Arya-Signal2MT4.user.js
+++ Arya-Signal2MT4.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Arya Signal to MT4 Script
 // @namespace    TamperMonkey
-// @version      1.1
-// @description  try to take over the world!
+// @version      1.2
+// @description  Simplify trade copy from arya signal! (Need an external software to save the parameters for MT4)
 // @author       Coding Seb
 // @match        https://arya.xyz/*
 // @require      http://code.jquery.com/jquery-3.4.1.min.js
@@ -23,13 +23,26 @@
                     if ($(element).children().length === 0) {
                         var content = $(element).text();
                         var parent = $(element).parent();
-                        var infosDiv = parent.parent().next().next().children()[0];
+                        var infosDiv = parent.parent();
+                        var tradeType = 'market';
+                        var parentText = infosDiv.parent().text().toUpperCase();
+                        if(parentText.includes('SELL STOP') || parentText.includes('BUY STOP'))
+                            tradeType = 'stop';
+                        if(parentText.includes('SELL LIMIT') || parentText.includes('BUY LIMIT'))
+                            tradeType = 'limit';
+
+                        while(!$(infosDiv).text().includes('SL:')){
+                            if(tradeType == 'market' && $(infosDiv).text().includes('CONDITION')){
+                                tradeType = 'limit';
+                            }
+                            infosDiv = infosDiv.next();
+                        }
                         console.log(infosDiv);
                         var infos = $(infosDiv).text();
                         console.log(infos);
                         var symbolDiv = parent.next();
                         var symbol = $(symbolDiv).text().trim();
-                        var trade = "trade:Type:market Symbole:[" + symbol + "] " + infos;
+                        var trade = "trade:Type:" + tradeType + " Symbole:[" + symbol + "] " + infos;
                         console.log(trade);
                         parent.replaceWith("<button onclick='window.open(\"" + trade + "\");' class='" + parent.attr('class') + "'>" + content.toUpperCase() + "</button>");
                     }
